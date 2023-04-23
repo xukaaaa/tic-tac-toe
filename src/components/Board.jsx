89d0d06ea1fe11f1fd3c58e48af881450d434fc7
18020331vnu <18020331@vnu.edu.vn>
2023-04-23 15:35:54 +0700

    add checkWinnerDiagonal fn

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -23,7 +23,7 @@ export default function Board() {
 
    const checkWinnerHorizontal = () => {
       for (let row = 0; row < 9; row++) {
-         for (let col = 0; col < 9; col++) {
+         for (let col = 0; col < 5; col++) {
             if (
                board[row][col] != '' && // Nếu ô đó không rỗng
                board[row][col] === board[row][col + 1] &&
@@ -39,9 +39,7 @@ export default function Board() {
 
    const checkWinnerVertical = () => {
       for (let col = 0; col < 9; col++) {
-         // Duyệt từng cột
-         for (let row = 0; row < 9; row++) {
-            // Duyệt từng dòng trong cột
+         for (let row = 0; row < 5; row++) {
             if (
                board[row][col] != '' && // Nếu ô đó không rỗng
                board[row][col] === board[row + 1][col] &&
@@ -55,14 +53,46 @@ export default function Board() {
       }
    }
 
+   const checkWinnerDiagonal = () => {
+      for (let row = 4; row < 9; row++) {
+         for (let col = 4; col < 9; col++) {
+            if (
+               board[row][col] &&
+               board[row][col] === board[row - 1][col - 1] &&
+               board[row][col] === board[row - 2][col - 2] &&
+               board[row][col] === board[row - 3][col - 3] &&
+               board[row][col] === board[row - 4][col - 4]
+            ) {
+               return board[row][col]
+            }
+         }
+      }
+
+      for (let row = 0; row < 5; row++) {
+         for (let col = 0; col < 5; col++) {
+            if (
+               board[row][col] &&
+               board[row][col] === board[row + 1][col + 1] &&
+               board[row][col] === board[row + 2][col + 2] &&
+               board[row][col] === board[row + 3][col + 3] &&
+               board[row][col] === board[row + 4][col + 4]
+            ) {
+               return board[row][col]
+            }
+         }
+      }
+   }
+
    const checkWinner = () => {
-      return checkWinnerHorizontal() || checkWinnerVertical()
+      return checkWinnerHorizontal() || checkWinnerVertical() || checkWinnerDiagonal()
    }
 
    useEffect(() => {
       if (checkWinner()) {
          console.log('Winner: ', checkWinner())
          setWinner(checkWinner())
+      } else if (!checkWinner() && board.every((row) => row.every((col) => col !== ''))) {
+         setWinner('Draw')
       }
    }, [board])
 
@@ -87,30 +117,34 @@ export default function Board() {
 
    return (
       <>
-         {board.map((arr, row) => {
-            return (
-               <div className="flex justify-center" key={row}>
-                  {arr.map((square, col) => {
-                     return (
-                        <Square
-                           value={square}
-                           key={row * 9 + col}
-                           onClick={() => {
-                              clickSquare(row, col)
-                           }}
-                        />
-                     )
-                  })}
-               </div>
-            )
-         })}
+         {winner === 'Draw' && <p>Draw</p>}
+         {winner && winner !== 'Draw' && <p>Winner: {winner}</p>}
+         <div className="border border-black">
+            {board.map((arr, row) => {
+               return (
+                  <div className="flex justify-center" key={row}>
+                     {arr.map((square, col) => {
+                        return (
+                           <Square
+                              value={square}
+                              key={row * 9 + col}
+                              onClick={() => {
+                                 clickSquare(row, col)
+                              }}
+                           />
+                        )
+                     })}
+                  </div>
+               )
+            })}
+         </div>
          {winner && (
             <button
                onClick={() => {
                   resetBoard()
                }}
             >
-               Play
+               Play again
             </button>
          )}
       </>
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 
 export default function Square({ value, onClick }) {
    return (
-      <button onClick={onClick} className="w-12 h-12 border border-black rounded-none ring-none">
+      <button onClick={onClick} className="w-12 h-12 border border-slate-300 rounded-none ring-none focus:outline-none hover:border-green-400">
          {value}
       </button>
    )
